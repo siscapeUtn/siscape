108cb545db0c5ab51e3f4cc8865dc8054a6a5b8c
siscapeUtn <siscape.utn@gmail.com>
2016-07-13 15:44:54 -0600

    cambios de daniel

--- Entities/Course.cs
+++ Entities/Course.cs
@@ -11,7 +11,8 @@ namespace Entities
         public String description { set; get; }
         public Program oProgram { set; get; }
         public Int16 state { set; get; }
-
+        public String schedule { get; set; }
+        public String days { get; set; }
         public Course() {
             oProgram = new Program();
         }
