97502b73e9427ef9a33c24ca00c4f142a9838b8a
siscapeUtn <siscape.utn@gmail.com>
2016-07-14 11:12:34 -0600

    fixing issues

--- siscape/DAL/Database/Stored Procedures/SystemModule_SP.sql
+++
@@ -1,9 +0,0 @@
-
-USE [ProUTN]
-GO
-
-INSERT INTO SYSTEMMODULE (SYSTEMMODULES_ID,DESCRIPTION,STATE,DELETED)
-VALUES (1,'Administracion',1,1),(2,'Seguridad',1,1), (3,'Servicios',1,1),(4,'Academico',1,1),(5,'Oferta',1,1)
-GO
-
-
