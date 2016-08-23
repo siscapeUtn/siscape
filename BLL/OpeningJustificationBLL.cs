3fc4cbe6c9718b6c371240d9238ce10c8aa29c2b
Rudy Sanchez <rsanchez@mybookie.cr>
2016-08-23 14:09:18 -0600

    fix merge

---
+++ BLL/OpeningJustificationBLL.cs
@@ -0,0 +1,79 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BLL
+{
+    public class OpeningJustificationBLL
+    {
+
+        private static OpeningJustificationBLL instace = null;
+
+        public static OpeningJustificationBLL getInstance()
+        {
+            if (instace == null)
+            {
+                instace = new OpeningJustificationBLL();
+            }
+            return instace;
+        }
+
+        public OpeningJustification getJustification(Int32 pCode)
+        {
+            String sql = "SP_OPENINGJUSTIFCATION";
+
+            try
+            {
+                SqlCommand oCommand = new SqlCommand(sql);
+                oCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                oCommand.Parameters.AddWithValue("@ID", pCode);
+                DataTable oDataTable = DAO.getInstance().executeQuery(oCommand);
+                OpeningJustification oJustification = new OpeningJustification();
+                AcademicOffer oAcademic = new AcademicOffer();
+                Teacher oTeacher = new Teacher();
+                Course oCourse = new Course();
+                InternalDesignation oInternal = new InternalDesignation();
+                foreach (DataRow oDataRow in oDataTable.Rows)
+                {
+                    oAcademic.code = Convert.ToInt32(oDataRow[0].ToString());
+                    oTeacher.name = oDataRow[1].ToString();
+                    oTeacher.lastName = oDataRow[2].ToString();
+                    oInternal.description= oDataRow[3].ToString();
+                    oInternal.baseSalary = Convert.ToDouble(oDataRow[4].ToString());
+                    oInternal.annuity = Convert.ToDouble(oDataRow[5].ToString());
+                    oAcademic.price = Convert.ToDecimal(oDataRow[6].ToString());
+                    oAcademic.hours = Convert.ToInt32(oDataRow[7].ToString());
+                    oCourse.description= oDataRow[8].ToString();
+                    oInternal.code= Convert.ToInt32(oDataRow[9].ToString());
+                    oAcademic.oteacher = oTeacher;
+                    oAcademic.oCourse = oCourse;
+                    oJustification.Anuality = 0;
+                    oJustification.CauntAnualities = 0;
+                    oJustification.CCSS = 0;
+                    oJustification.Diference = 0;
+                    oJustification.Others = 0;
+                    oJustification.publicity = 0;
+                    oJustification.Salary = 0;
+                    oJustification.Students = 0;
+                    oJustification.TotalBimensual = 0;
+                    oJustification.TotalIncome = 0;
+                    oJustification.TotalMensual = 0;
+                    oJustification.oInternal = oInternal;
+                    oJustification.oAcademic = oAcademic;
+
+                }
+                return oJustification;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { }
+        }
+    }
+}
---
+++ Entities/OpeningJustification.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entities
+{
+    public class OpeningJustification
+    {
+        public AcademicOffer oAcademic { get; set; }
+        public InternalDesignation  oInternal { get; set; }
+        public double Salary { get; set; }
+        public double CauntAnualities { get; set; }
+        public double Anuality { get; set; }
+        public double CCSS { get; set;}
+        public double Others { get; set;}
+        public double publicity { get; set; }
+        public double TotalMensual { get; set; }
+        public double TotalBimensual { get; set; }
+        public int Students {get; set;}
+        public double TotalIncome { get; set; }
+        public double Diference { get; set; }
+
+        public OpeningJustification() { }
+
+        public void getSalary()
+        {
+            double hours = TypesCategories(this.oAcademic.hours);
+            this.Salary = oInternal.baseSalary * hours;
+        }
+
+        public void getCCSS()
+        {
+            double cant= 0.37;
+            this.CCSS = cant*this.Salary;
+            this.publicity = 17000;
+        }
+
+        public void getCalc(int anuality,double others, int students)
+        {
+            this.Others = others;
+            this.CauntAnualities = anuality;
+            this.Students = students;
+            calcAnuality();
+            calcIncomes();
+            calcIncomesMonth();
+            totalDiference();
+        }
+
+        private void totalDiference()
+        {
+            this.Diference = (this.TotalIncome - this.TotalBimensual);
+        }
+
+        private void calcIncomesMonth()
+        {
+            double total = 0;
+            total = this.Salary + this.Anuality + this.CCSS + this.publicity + this.Others;
+            this.TotalMensual = total;
+            this.TotalBimensual = total * 2;
+        }
+
+        private void calcAnuality()
+        {
+            double cant = this.CauntAnualities * oInternal.annuity;
+            double hours= TypesCategories(this.oAcademic.hours);
+            this.Anuality = cant * hours;
+        }
+
+        private void calcIncomes()
+        {
+            this.TotalIncome =Convert.ToDouble(oAcademic.price * this.Students);
+        }
+
+        public double TypesCategories(int pHours)
+        {
+            double total = 0;
+            switch (pHours)
+            {
+                case 5: total =0.133333333333333;
+                        break;
+                case 10:
+                        total = 0.266666666666667;
+                        break;
+                case 15:
+                        total =0.4;
+                        break;
+                case 20:
+                        total = 0.533333333333333;
+                        break;
+                case 25:
+                        total = 0.666666666666667;
+                        break;
+                case 30:
+                        total = 0.8;
+                        break;
+                case 35:
+                    total = 0.933333333333333;
+                    break;
+                case 40:
+                    total = 1.066666666666667;
+                    break;
+            }
+            return total;
+        }
+
+
+    }
+}
